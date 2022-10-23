19585b032e6c09815f9fd5d800ecf671f95b0ff8
abady2001 <61017857+abady2001@users.noreply.github.com>
2022-10-22 17:05:46 -0700

    Add files via upload

---
+++ huda/js/home_page.js
@@ -0,0 +1,32 @@
+
+// window.onscroll = function () {
+
+//     let stars = document.getElementById('stars');
+//     let moon = document.getElementById('moon');
+//     let mountains3 = document.getElementById('mountains3');
+//     let mountains4 = document.getElementById('mountains4');
+//     let river = document.getElementById('river');
+//     let boat = document.getElementById('boat');
+//     let huda = document.querySelector('.huda');
+
+//     let value = scrollY;
+
+//     console.log(huda);
+
+
+//     stars.style.left = value + 'px';
+//     moon.style.top = value * 3 + 'px';
+//     mountains3.style.top = value * 2 + 'px';
+//     mountains4.style.top = value * 1.5 + 'px';
+//     river.style.top = value + 'px';
+//     boat.style.left = value * 3 + 'px';
+//     huda.style.fontSize = value + 'px';
+
+//     if (scrollY > 180) {
+//         huda.style.fontSize = 180 + 'px';
+//         huda.style.postion = 'fixed';
+
+//     }
+
+
+// }
